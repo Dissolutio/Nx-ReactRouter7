3d440b24a8cd42603f7a990f93dbdfc893e4b5cc
John M <entity.john@gmail.com>
2025-06-27 10:50:33 -0500

    errors I can't fix, wrong path somehow

--- react-apps/monoquest/app/app.tsx
+++ react-apps/monoquest/app/app.tsx
@@ -1,6 +1,10 @@
+
+import '@shadcn-test/styles/global.css';
+import { Button } from '@nx-react-router7/ui'
 export function App() {
   return (
-    <div data-testid="app" className="bg-indigo-500 p-2 font-mono">Hello!</div>
+    <div data-testid="app" className="bg-indigo-500 p-2 font-mono">Hello!
+      <Button /></div>
   );
 }
 
